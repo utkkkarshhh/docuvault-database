9ff045de9c68b91845de28fbdf08b77faeb195c6
Utkarsh Bhardwaj <utkarshbhardwajmail@gmail.com>
2024-12-15 17:45:28 +0530

    updated index

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-const { Sequelize } = require('sequelize');
+const {Sequelize} = require('sequelize');
 const config = require('./config/config');
 
-const sequelize = new Sequelize(
-    config.development.database,
-    config.development.username,
-    config.development.password,
-    {
-      host: config.development.host,
-      dialect: "postgres",
-      port: config.development.port,
-    }
-  );
+const sequelize = new Sequelize(config.development.database, config.development.username, config.development.password, {
+    host: config.development.host, dialect: "postgres", port: config.development.port,
+});
 
 const documents = require('./models/document')(sequelize);
-const users = require('./models/users')(sequelize);
-const users = require('./models/publicVisibility')(sequelize);
-const users = require('./models/userDetail')(sequelize);
-const users = require('./models/userLimit')(sequelize);
-const users = require('./models/userLogin')(sequelize);
+const publicVisibility = require('./models/publicVisibility')(sequelize);
+const userDetail = require('./models/userDetail')(sequelize);
+const userLimit = require('./models/userLimit')(sequelize);
+const userLogin = require('./models/userLogin')(sequelize);
 
 module.exports = {
-  sequelize,
-  models: {
-    documents,
-    users,
-  },
+    sequelize, models: {
+        documents, publicVisibility, userDetail, userLimit, userLogin
+    },
 };
